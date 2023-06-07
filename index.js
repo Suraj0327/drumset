4ae2bb4f4bcc2c1f6e3b8ce2a09db2d9b3f8a376
Suraj0327 <91056405+Suraj0327@users.noreply.github.com>
2023-06-07 11:40:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-// var audio=new Audio('crash.mp3');
-
-// for(var i=0;i<document.querySelectorAll(".drum").length;i++){
-  
-//     document.querySelectorAll(".drum")[i].addEventListener("click",function() {
-//     //    audio.play();
-//     this.
-//     })
-//     }
-    
-
-
-// Create an object to map button classes to their corresponding audio files
-var audioMap = {
-    w: 'tom-1.mp3',
-    a: 'tom-2.mp3',
-    s: 'tom-3.mp3',
-    d: 'tom-4.mp3',
-    j: 'kick-bass.mp3',
-    k: 'snare.mp3',
-    l: 'crash.mp3',
-   
-   
-  };
-  
-  // Function to play the corresponding sound based on the button class
-  function playSound(buttonClass) {
-    var audio = new Audio(audioMap[buttonClass]);
-    audio.play();
-  }
-  
-  // Add event listeners to the drum buttons
-  document.querySelectorAll(".drum").forEach(function(button) {
-    button.addEventListener("click", function() {
-      var buttonClass = this.innerText;
-      playSound(buttonClass);
-    });
+for (var i = 0; i < document.querySelectorAll(".drum").length; i++) {
+  document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+    var buttonInnerHtml = this.innerHTML;
+    switch (buttonInnerHtml) {
+      case "w":
+        var tom1 = new Audio("tom-1.mp3");
+        tom1.play();
+        break;
+      case "a":
+        var tom2 = new Audio("tom-2.mp3");
+        tom2.play();
+        break;
+      case "s":
+        var tom3 = new Audio("tom-3.mp3");
+        tom3.play();
+        break;
+      case "d":
+        var tom4 = new Audio("tom-4.mp3");
+        tom4.play();
+        break;
+      case "j":
+        var kick = new Audio("kick-bass.mp3");
+        kick.play();
+        break;
+      case "k":
+        var snare = new Audio("snare.mp3");
+        snare.play();
+        break;
+      case "l":
+        var crash = new Audio("crash.mp3");
+        crash.play();
+        break;
+      default:
+        console.log(buttonInnerHtml);
+    }
   });
+}
+
   
